34d2b102744051c55f1f7ea8eb2b9cbddad23d9a
hariharan-a-16 <haran4889@gmail.com>
2026-01-06 11:24:21 +0530

    Successfully Added

--- Eshop/mainapp/views.py
+++ Eshop/mainapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from .models import CarouselImage
+
 # Create your views here.
 
 def homeView(request):
     template = 'mainapp/home.html'
-    context = {}
+    context = {
+
+        'current_page' : 'home',
+
+        #Let's collect all existing records of carousel image table to be sent yo template
+        'carousel_images' : CarouselImage.objects.all() # Select * from carousel_image;
+    }
 
     return render(request, template_name= template, context= context)
 
@@ -12,14 +20,18 @@ def homeView(request):
 
 def aboutView(request):
     template = 'mainapp/about.html'
-    context = {}
+    context = { 
+        'current_page' :'about'
+    }
 
     return render(request, template_name= template, context= context)
 
 
 def contactView(request):
     template = 'mainapp/contact.html'
-    context = {}
+    context = {
+        'current_page' : 'contact'
+    }
 
     return render(request, template_name= template, context= context)
 
