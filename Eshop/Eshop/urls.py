8f1e1d929ce0f64d74bd5f225a93b9cbd225700d
hariharan-a-16 <haran4889@gmail.com>
2026-01-07 12:10:28 +0530

    Updated

--- Eshop/Eshop/urls.py
+++ Eshop/Eshop/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls'))
+    path('', include('mainapp.urls')),
+    path('products/', include('products.urls'))
+
 ]
 
 
--- Eshop/mainapp/urls.py
+++ Eshop/mainapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns =[
     path('',views.homeView, name='home_page'),
     path("about/",views.aboutView, name='about_page'),
     path("contact/",views.contactView,name='contact_page')
+    
 ]
 
 
