c7da2f836848624a3cdf35fe738e70a5baf5c365
hariharan-a-16 <haran4889@gmail.com>
2026-01-28 23:21:35 +0530

    Updated

--- Eshop/mainapp/urls.py
+++ Eshop/mainapp/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
 
 from . import views 
+from .views import (
+    CarouselImageCreateView,
+    CarouselImageListView,
+    CarouselImageUpdateView,
+    CarouselImageDeleteView
+)
 
 
 urlpatterns =[
     path('',views.homeView, name='home_page'),
     path("about/",views.aboutView, name='about_page'),
-    path("contact/",views.contactView,name='contact_page')
+    path("contact/",views.contactView,name='contact_page'),
+
+    path('carousels/', CarouselImageListView.as_view(), name='carousel_list'),
+    path('carousels/add/', CarouselImageCreateView.as_view(), name='carousel_add'),
+    path('carousels/<int:pk>/edit/', CarouselImageUpdateView.as_view(), name='carousel_edit'),
+    path('carousels/<int:pk>/delete/', CarouselImageDeleteView.as_view(), name='carousel_delete'),
     
 ]
 
